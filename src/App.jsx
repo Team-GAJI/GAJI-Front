13743f52430b38ceb1b1530fde5dbfd450f9f0aa
jiminseong <iamjms4237@gmail.com>
2024-07-12 03:08:44 +0900

    feat:#5 메인 레이아웃
    헤더와 푸터로 구성된 메인레이아웃 구현, Color.jsx, Logo.jsx 두가지 컴포넌트 구현
    
    Resolves: #5

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'; 
 import LoginPage from './pages/LoginPage';
+import MainLayOut from './layout/MainLayOut';
+import CommunityPage from './pages/CommunityPage';
 
 
 function App() {
   return (
     <>
     <Routes>
-      <Route exact path="/" element={<LoginPage/>}/>
+    <Route exact path="/" element={<LoginPage/>}/>
+      <Route element={<MainLayOut/>}>
+      <Route exact path="/community" element={<CommunityPage/>}/>
+      </Route>
     </Routes>
     </>
   )
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import Logo from './Logo';
+import { Color } from '../style/Color';
+
+const Footer = () => {
+    return (
+        <FooterWrapper>
+            <RowWrapper>
+                <Text>홈</Text>
+                <Text>스터디</Text>
+                <Text>커뮤니티</Text>
+                <Text>로드맵</Text>
+                <Text>강의</Text>
+                <Text>마이페이지</Text>
+            </RowWrapper>
+            <RowLogoWrapper>
+                <Logo/>
+                <LogoText>가지고 싶은 스터디, <Color>GAJI</Color></LogoText>
+            </RowLogoWrapper>
+            <FooterText>@ Copyright 2024_GAJI</FooterText>
+        </FooterWrapper>
+    );
+};
+
+export default Footer;
+
+const FooterWrapper = styled.div`
+    background-color : #E8E9EC;
+    width : 100%;
+    height : 277px;
+    box-sizing: border-box; 
+    display  : flex;
+    flex-direction : column;
+    align-items : center;
+    justify-content : center;
+    gap : 3.75em;
+    
+
+`;
+
+const RowWrapper = styled.div`
+    display : flex;
+    gap :  3.75em;
+    justify-content : center;
+    align-items : center;
+`
+
+const RowLogoWrapper = styled(RowWrapper)`
+    gap : 1.5em;
+`;
+
+const Text = styled.div`
+    color : #697077;
+    font-size : 0.8125em;
+    font-size : 400;
+`;
+
+const LogoText = styled.div`
+    font-size : 1em;
+    font-weight : 700;
+`
+
+const FooterText = styled.div`
+    color : #959595;
+    font-size : 0.8125em;
+    font-wiehgt: 700;
+`;
