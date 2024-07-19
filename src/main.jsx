ce5d008b03a83403c29d45e43493a32b920ba413
Kimtaeyeon04 <te4939@naver.com>
2024-07-19 19:53:03 +0900

    스터디 상세보기 레이아웃

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux'
 import {store} from './store/store.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom'
-import StudyRoom from './pages/StudyRoom.jsx';
+import StudyDetail from './pages/StudyDetail.jsx';
+/*import User from './pages/User.jsx';*/
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <StudyRoom/>
+    <StudyDetail/>
     <BrowserRouter>
     <Provider store={store}>
     <App />
