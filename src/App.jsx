d2f73df9c0f35b57e656ab278b075f30da57a9a4
Kimtaeyeon04 <te4939@naver.com>
2024-07-29 20:48:16 +0900

    스터디 둘러보기 레이아웃 수정 및 파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainLayOut from './layout/MainLayOut';
 import CommunityPage from './pages/CommunityPage';
-
+import StudyCategory from './pages/StudyCategory/StudyCategory';
 
 function App() {
   return (
     <>
     <Routes>
     <Route exact path="/" element={<LoginPage/>}/>
-      <Route element={<MainLayOut/>}>
+      <Route>
       <Route exact path="/community" element={<CommunityPage/>}/>
+      <Route exact path="/studycategory" element={<StudyCategory/>}/>
       </Route>
     </Routes>
     </>
