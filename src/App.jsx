14fdd934bbbac7a1055733806595896c4ed32782
jiminseong <iamjms4237@gachon.ac.kr>
2024-08-01 15:28:11 +0900

    스터디룸 기능

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,29 @@ import { Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import MainLayOut from './layout/MainLayOut';
 import CommunityPage from './pages/CommunityPage';
+import StudyCategory from './pages/StudyCategory/StudyCategory';
 import CommunityPostPage from './pages/CommunityPostPage';
 import CommunityWritePage from './pages/CommunityWritePage';
 import MyPage from './pages/MyPage';
-import StudyRoom from './pages/StudyRoom';
-import StudyCategory from './pages/StudyCategory';
-import Notice from './pages/NoticeRoom/NoticeRoom.jsx';
+import StudyRoom from './pages/StudyRoom/StudyRoom.jsx';
 
 
 function App() {
   return (
-    <>
     <Routes>
-    <Route path="/login" element={<LoginPage/>}/>
       <Route element={<MainLayOut/>}>
-      <Route path="/" element={<CommunityPage/>}/>
-      <Route path="/community" element={<CommunityPage/>}/>
-      <Route path="/community/post" element={<CommunityPostPage/>}/>
-      <Route path="/community/write" element={<CommunityWritePage/>}/>
-      <Route path="/mypage" element={<MyPage/>}/>
-      <Route path="/studyroom" element={<StudyRoom/>}/>
-      <Route path="/study" element={<StudyCategory/>}/>
-      <Route exact path="/notice" element={<Notice/>}/>
-
+        <Route exact path="/login" element={<LoginPage/>}/>
+        <Route exact path="/studyroom" element={<StudyRoom/>}/>      
+        <Route path="/" element={<CommunityPage/>}/>
+        <Route path="/community" element={<CommunityPage/>}/>
+        <Route path="/community/post" element={<CommunityPostPage/>}/>
+        <Route path="/community/write" element={<CommunityWritePage/>}/>
+        <Route path="/mypage" element={<MyPage/>}/>
+        <Route path="/studyroom" element={<StudyRoom/>}/>
+        <Route path="/study" element={<StudyCategory/>}/>
     </Route>
     </Routes>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
