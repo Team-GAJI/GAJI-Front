face76bb307400b7eb499787f20604228cb6384b
Kimtaeyeon04 <te4939@naver.com>
2024-08-13 17:25:36 +0900

    API 수정

--- src/utils/studyManage/studyManageDateAPI.jsx
+++ src/utils/studyManage/studyManageDateAPI.jsx
@@ -1,9 +1,10 @@
-import { apiBase } from "../API";
+import { api } from "../API";
 
 export const studyManageDateAPI = async (roomId, Weeks, userId) => {
     try {
-        const response = await apiBase.get(`/api/studyRooms/event/${roomId}/${Weeks}/${userId}/period`);
-        console.log(response);
+        const response = await api.get(`/api/studyRooms/event/${roomId}/${Weeks}/${userId}/period`);
+        console.log(response)
+
     } catch (error) {
         console.error('API 요청 중 오류 발생:', error);
         throw error; 
