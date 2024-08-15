3cf7a2354ad6e262ae2ec9a4e4c1329b37054801
jiminseong <iamjms4237@gachon.ac.kr>
2024-08-15 16:30:55 +0900

    WritePost 반응형 구현 완료

--- src/components/common/MediaWrapper.jsx
+++ src/components/common/MediaWrapper.jsx
@@ -12,3 +12,17 @@ export const ContentWrapper = styled.div`
     }
 
 `
+
+
+export const ContentWrapper60 = styled.div`
+    display : flex;
+    flex-direction : column;
+    align-items : center;
+    width : 60%;
+    margin : 0 auto;
+    
+    @media(max-width: 768px) {
+        width : 90%;
+    }
+
+`
