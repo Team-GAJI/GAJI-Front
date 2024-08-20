ee1bb8d5338b11d1bd761ea1ad759f1f93f9eb64
ehgus0526 <ehgus0526@naver.com>
2024-08-20 15:57:15 +0900

    커뮤니티 작성, 상세보기 API 연결

--- src/utils/communityWrite/communityWriteAPI.jsx
+++ src/utils/communityWrite/communityWriteAPI.jsx
@@ -3,9 +3,8 @@ import { api } from "../API";
 export const communityWriteAPI = async (data) => {
     try {
         const response = await api.post(`community-posts`, data);
-        console.log(response)
-        console.log(response.result)
-        return response.result
+        console.log(response.data.result.postId)
+        return response.data.result.postId;
     } catch (error) {
         console.error('API 요청 중 오류 발생:', error);
         throw error; 
