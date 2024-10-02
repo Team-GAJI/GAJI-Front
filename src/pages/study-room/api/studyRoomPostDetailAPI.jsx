e2359e615c07d41d0baa97fdb15c59d1b7956ec2
jiminseong <iamjms4237@gachon.ac.kr>
2024-10-02 15:59:00 +0900

    refactor: SideBar 분리

--- src/pages/study-room/api/studyRoomPostDetailAPI.jsx
+++ src/pages/study-room/api/studyRoomPostDetailAPI.jsx
@@ -1,15 +1,13 @@
-import { api } from "../../../app/api";
+import { api } from '../../../app/api';
 
 export const studyRoomPostDetailAPI = async (postId) => {
     try {
-        console.log(postId)
+        console.log(postId);
         const response = await api.get(`study-rooms/post/${postId}detail`);
         console.log(response.data);
         return response.data.result;
-
     } catch (error) {
         console.error('API 요청 중 오류 발생:', error);
-        throw error; 
+        throw error;
     }
 };
-
--- src/pages/study-room/api/studyRoomWriteAPI.jsx
+++ src/pages/study-room/api/studyRoomWriteAPI.jsx
@@ -1,15 +1,12 @@
-import { api } from "../../../app/api";
+import { api } from '../../../app/api';
 
-export const studyRoomWriteAPI = async (roomId,data) => {
+export const studyRoomWriteAPI = async (roomId, data) => {
     try {
-        const response = await api.post(`study-rooms/post/${roomId}`,data);
+        const response = await api.post(`study-rooms/post/${roomId}`, data);
         console.log(response.data);
-        return response.data
-
+        return response.data;
     } catch (error) {
         console.error('API 요청 중 오류 발생:', error);
-        throw error; 
+        throw error;
     }
 };
-
-
